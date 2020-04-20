433036f1804aa12f97c2e9f4493366249a5bfb25
Joe Reiter <joeyreiter27@gmail.com>
2020-04-19 22:32:52 -0400

    Create multiple methods within a class and implement them in Program file.

---
+++ ConsoleApp2/NumberGuesser.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    /// <summary>
+    /// This program askes the user to think of a number, and the program figures that number out in 8 or less guesses
+    /// </summary>
+    public class NumberGuesser
+    {
+
+        #region Public Properties 
+        /// <summary>
+        /// The largest number the user can pick
+        /// </summary>
+        public int MaximumNumber { get; set; }    // variable turned into a public property, not given a private set because 
+
+        /// <summary>
+        /// The current number of guesses the computer is at
+        /// </summary>
+        public int CurrentNumberOfGuesses { get; private set; } // dont need to assign your other properties a value here because the program will assign them a new value each time the program is run
+
+
+        /// <summary>
+        /// the current minimum value of the range that the computer is asking
+        /// </summary>
+        public int GuessMinimum { get; private set; } // ***private set used here so the value can only be set inside of this class***
+
+          /// <summary>
+          /// the current max value of the range that the computer is asking
+          /// </summary>
+        public int GuessMaximum { get; private set; }
+        #endregion
+
+
+        #region .ctor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public NumberGuesser()
+        {
+            //Set default max number 
+            this.MaximumNumber = 100;
+        }
+        #endregion
+
+
+        #region Methods 
+        /// <summary>
+        /// Asks the user to think of a number between 0 and max number
+        /// </summary>
+        public void InformUser()
+        {
+
+            // ask user to think of number
+            Console.Write($"Think of a number between 0 and {this.MaximumNumber}"); ;
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Asks the user a series of questions to discover the number theyre thinking of
+        /// </summary>
+        public void DiscoverNumber()
+        {
+
+            //clear variable to inital value before any Discover function is used. 
+            this.GuessMaximum = this.MaximumNumber / 2;
+            this.CurrentNumberOfGuesses = 0;
+            this.GuessMinimum = 0;
+
+            while (this.GuessMinimum != this.GuessMaximum)
+            {
+                //Increase guess amount
+                this.CurrentNumberOfGuesses++;
+
+                //Ask the user if their number is between the guess range
+                Console.WriteLine($"Is your number between {this.GuessMinimum} and {this.GuessMaximum}?");
+                string response = Console.ReadLine();
+
+
+                //if the user confirmed their number is within the current range
+                //    bool hasReponse  = (response != null && response.Length > 0);
+                //   if ( response[0] == 'y' || response[0] == 'Y')
+
+
+                // ? after the variable checks to see if variable !=` null  .... this ensures your variable is populated with something
+                //ToLower() converts the variable to lowercase so it will check both upper and lower in the if statement
+                if (response?.ToLower().FirstOrDefault() == 'y')
+                {
+                    //we know the number is between guessFrom and guessTo
+                    //So set the new max number
+                    this.MaximumNumber = this.GuessMaximum;
+
+                    // change the next guess range to be half of the new max range
+                    this.GuessMaximum = this.GuessMaximum - (this.GuessMaximum - this.GuessMinimum) / 2;
+
+
+
+                }
+                // the number is greater than guessMax and less than or equal to Max
+                else
+                {
+                    //the new min is one above the old maximum
+                    this.GuessMinimum = this.GuessMaximum + 1;
+
+                    //Guess the bottom half of the new range 
+                    int remainingDiff = this.MaximumNumber - this.GuessMaximum;
+
+                    // add the remaining diff to the guessMax for your next ask
+                    // NOTE: Math.Ceiling will round up the remaining difference to 2, if the difference is 3.
+                    this.GuessMaximum += (int)Math.Ceiling(remainingDiff / 2f);
+
+                }
+
+                // if we only  have two numbers left we guess one of them
+                if (this.GuessMinimum + 1 == this.MaximumNumber)
+                {
+                    //increase guesses by 1
+                    this.CurrentNumberOfGuesses++;
+                    // guess the minimum
+                    Console.WriteLine($"Is your number {this.GuessMinimum}?");
+                    response = Console.ReadLine();
+
+                    if (response?.ToLower().FirstOrDefault() == 'y')
+                    {
+                        break;  // breaks out of the while loop
+                    }
+                    else
+                    {
+                        //assigns your guessMin for the output below, also sets the two equal so the while loop is broken out of
+                        this.GuessMinimum = this.MaximumNumber;
+                    }
+
+
+                }
+
+            }// end of while loop
+        }
+
+        /// <summary>
+        /// Announces results of guesser
+        /// </summary>
+        public void AnnouceResult()
+        {
+            Console.WriteLine($"Your number is: {this.GuessMinimum} and it took {CurrentNumberOfGuesses} guesses to figure it out.");
+            Console.ReadLine();
+        }
+       #endregion
+    
+    }
+      
+
+     
+}
+
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            #region Notes
             /*
             byte myByte = 255;      //8 bit maxes out at 255
             char myChar = '2';      // 16 bits, can only be one character 
@@ -59,93 +60,23 @@ namespace ConsoleApp2
 
             Console.ReadLine();  
             */
+            #endregion
 
+            //creates an instance of the NumberGuesser class
+            var numberGuesser = new NumberGuesser(); 
 
-            // ask user to think of number
-            Console.Write("Think of a number between 0 and 100"); ;
-            Console.ReadLine();
+            //Change the default max to 200
+            numberGuesser.MaximumNumber = 200;
 
-            // defines the initial max number the user can guess
-            int max = 100;
+            //Call the InformUser method to ask the user to think of a number
+            numberGuesser.InformUser();
 
-            //keep track of the number of guesses
-            int guesses = 0;
+            //Call the DiscoverNumber method to continually ask the user a range of values
+            numberGuesser.DiscoverNumber();
 
-            //the start guess from
-            int guessMin = 0;
+            //Announce the users number
+            numberGuesser.AnnouceResult();
 
-            // the start guess to (half of the max)
-            int guessMax = max / 2;
-
-            // while the guess isnt the same as the known max value
-            while (guessMin != max)
-            {
-                //Increase guess amount
-                guesses++;
-                //Ask the user if their number is between the guess range
-                Console.WriteLine($"Is your number between {guessMin} and {guessMax}?");
-                string response = Console.ReadLine();
-
-
-                //if the user confirmed their number is within the current range
-                //    bool hasReponse  = (response != null && response.Length > 0);
-                //   if ( response[0] == 'y' || response[0] == 'Y')
-                
-
-                // ? after the variable checks to see if variable != null  .... this ensures your variable is populated with something
-                //ToLower() converts the variable to lowercase so it will check both upper and lower in the if statement
-                if(response?.ToLower().FirstOrDefault() == 'y' ) 
-                {
-                    //we know the number is between guessFrom and guessTo
-                    //So set the new max number
-                    max = guessMax;
-
-                    // change the next guess range to be half of the new max range
-                    guessMax = guessMax - (guessMax - guessMin) / 2; 
-
-
-
-                }
-                // the number is greater than guessMax and less than or equal to Max
-                else
-                    {
-                    //the new min is one above the old maximum
-                    guessMin = guessMax + 1;
-
-                    //Guess the bottom half of the new range 
-                    int remainingDiff = max - guessMax;
-
-                    // add the remaining diff to the guessMax for your next ask
-                    // NOTE: Math.Ceiling will round up the remaining difference to 2, if the difference is 3.
-                    guessMax += (int) Math.Ceiling(remainingDiff / 2f);
-
-                    }
-
-                // if we only  have two numbers left we guess one of them
-                if (guessMin + 1 == max)
-                {
-                    //increase guesses by 1
-                    guesses++;
-                    // guess the minimum
-                    Console.WriteLine($"Is your number {guessMin}?");
-                    response = Console.ReadLine();
-
-                    if (response?.ToLower().FirstOrDefault() == 'y')
-                    {
-                        break;  // breaks out of the while loop
-                    }
-                    else
-                    {
-                        //assigns your guessMin for the output below, also sets the two equal so the while loop is broken out of
-                        guessMin = max; 
-                    }
-
-               
-                }
-           
-            }// end of while loop
-            Console.WriteLine($"** Your number {guessMin} was guessed in {guesses} guesses.**");
-            Console.ReadLine();
         }
     }
 }
